60a3bfe57d2824f009da54a3f82b1e5f2d1e09f4
Romain Siré <romain.sire@gmx.com>
2022-02-18 22:06:02 +0100

    feat: create dynamic page for one article

---
+++ src/routes/[slug].svelte
@@ -0,0 +1,28 @@
+<script context="module">
+	export async function load({ fetch, params }) {
+		const slug = params.slug;
+		console.log(slug);
+		const id = 12; //à changer !
+		const res = await fetch(`http://localhost:5000/api/articles/${id}`);
+		const article = await res.json();
+		if (res.ok) {
+			return {
+				props: {
+					article
+				}
+			};
+		} else {
+			return {
+				status: res.status,
+				error: new Error('Could not fetch the article')
+			};
+		}
+	}
+</script>
+
+<script>
+	export let article;
+	console.log(article);
+</script>
+
+<h1>Hello World</h1>
