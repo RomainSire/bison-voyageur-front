ea7de10a0fe9d4b8e677a405dbdf6601207ec2f7
Romain Siré <romain.sire@gmx.com>
2022-02-28 23:45:04 +0100

    feat: display and style the markdown with 'marked' library

--- src/routes/article/[slug].svelte
+++ src/routes/article/[slug].svelte
@@ -26,11 +26,44 @@
 </script>
 
 <script>
+	import { onMount } from 'svelte';
+	import { marked } from 'marked';
 	// article data from SSR
 	export let article;
-	console.log(article);
+	// replacing image url by filename, url is redefined below with other img tags modifications
+	let content = marked(article.body)
+		.replaceAll('/uploads/', `${backendUrl}/uploads/medium_`)
+		.replaceAll('<img', '<img loading="lazy"');
+
+	// onMount lifecycle handler: makes all the markdown pictures clickable
+	let images;
+	onMount(() => {
+		images = [...document.querySelectorAll('.markdown img')];
+		images.forEach((image) => {
+			image.addEventListener('click', onImageClicked);
+		});
+	});
+
+	// triggered when an image is clicked
+	function onImageClicked(e) {
+		const clickedImgIndex = images.findIndex((image) => {
+			return image.src === e.target.src;
+		});
+		console.log(clickedImgIndex);
+	}
 </script>
 
-<h1>{article.title}</h1>
-<h2>{article.date}</h2>
-<div>{article.body}</div>
+<div class="wrapper">
+	<h1>{article.title}</h1>
+	<h2>{article.date}</h2>
+	<div class="markdown">
+		{@html content}
+	</div>
+</div>
+
+<style lang="scss">
+	.wrapper {
+		padding: 2rem 5%;
+	}
+	// styles of the markdown are located in the ../../styles/global.css file. styles here are not applied
+</style>
