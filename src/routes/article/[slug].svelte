15c62b34cc3b04b3130f27f46b8d078291bd55ea
Romain Siré <romain.sire@gmx.com>
2022-03-04 13:56:46 +0100

    feat: add animated photo gallery

--- src/routes/article/[slug].svelte
+++ src/routes/article/[slug].svelte
@@ -27,7 +27,13 @@
 
 <script>
 	import { onMount } from 'svelte';
+	import { fade, slide } from 'svelte/transition';
 	import { marked } from 'marked';
+
+	/**
+	 * Convert and display the markdown article
+	 */
+
 	// article data from SSR
 	export let article;
 	// format date
@@ -38,6 +44,10 @@
 		.replaceAll('/uploads/', `${backendUrl}/uploads/medium_`)
 		.replaceAll('<img', '<img loading="lazy"');
 
+	/**
+	 * Image gallery management
+	 */
+
 	// onMount lifecycle handler: makes all the markdown pictures clickable
 	let images;
 	onMount(() => {
@@ -46,13 +56,39 @@
 			image.addEventListener('click', onImageClicked);
 		});
 	});
-
+	let isGalleryDisplayed = false;
 	// triggered when an image is clicked
+	let currentImgIndex;
 	function onImageClicked(e) {
-		const clickedImgIndex = images.findIndex((image) => {
+		currentImgIndex = images.findIndex((image) => {
 			return image.src === e.target.src;
 		});
-		console.log(clickedImgIndex);
+		updateImageSource();
+		isGalleryDisplayed = true;
+	}
+	// triggered when the next/back image button is clicked
+	function changeImage(direction) {
+		if (direction === 'next') {
+			if (currentImgIndex === images.length - 1) {
+				currentImgIndex = 0;
+			} else {
+				currentImgIndex++;
+			}
+		} else {
+			if (currentImgIndex === 0) {
+				currentImgIndex = images.length - 1;
+			} else {
+				currentImgIndex--;
+			}
+		}
+		updateImageSource();
+	}
+	// update src and alt attributes to display the image at the correct index
+	let src = '';
+	let alt = '';
+	function updateImageSource() {
+		src = images[currentImgIndex].src.replace('medium', 'large');
+		alt = images[currentImgIndex].alt;
 	}
 </script>
 
@@ -63,6 +99,75 @@
 		{@html content}
 	</div>
 </div>
+
+{#if isGalleryDisplayed}
+	<section class="gallery" transition:fade>
+		<button
+			class="gallery__btn gallery__close"
+			on:click={() => {
+				isGalleryDisplayed = false;
+			}}
+		>
+			<!-- close icon -->
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				aria-hidden="true"
+				role="img"
+				class="iconify iconify--mdi"
+				preserveAspectRatio="xMidYMid meet"
+				viewBox="0 0 24 24"
+				><path
+					fill="currentColor"
+					d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+				/>
+			</svg>
+		</button>
+		<button
+			class="gallery__btn gallery__next"
+			on:click={() => {
+				changeImage('next');
+			}}
+		>
+			<!-- next icon -->
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				aria-hidden="true"
+				role="img"
+				class="iconify iconify--ic"
+				width="32"
+				height="32"
+				preserveAspectRatio="xMidYMid meet"
+				viewBox="0 0 24 24"
+				><path fill="currentColor" d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z" /></svg
+			>
+		</button>
+		<button
+			class="gallery__btn gallery__back"
+			on:click={() => {
+				changeImage('back');
+			}}
+		>
+			<!-- back icon -->
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				aria-hidden="true"
+				role="img"
+				class="iconify iconify--ic"
+				width="32"
+				height="32"
+				preserveAspectRatio="xMidYMid meet"
+				viewBox="0 0 24 24"
+				><path fill="currentColor" d="M15.41 7.41L14 6l-6 6l6 6l1.41-1.41L10.83 12z" /></svg
+			>
+		</button>
+		{#key src}
+			<img {src} {alt} class="gallery__image" transition:slide />
+		{/key}
+	</section>
+{/if}
 
 <style lang="scss">
 	.wrapper {
@@ -76,4 +181,43 @@
 		}
 	}
 	// styles of the markdown are located in the ../../styles/global.css file. styles here are not applied
+
+	.gallery {
+		background: #000d;
+		position: fixed;
+		inset: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		&__btn {
+			position: absolute;
+			background: transparent;
+			border: none;
+			cursor: pointer;
+			padding: 1em;
+			svg {
+				width: 3rem;
+				height: 3rem;
+				color: #fff;
+			}
+		}
+		&__close {
+			top: 0;
+			right: 0;
+		}
+		&__next {
+			right: 0;
+			top: 50%;
+			transform: translateY(-50%);
+		}
+		&__back {
+			left: 0;
+			top: 50%;
+			transform: translateY(-50%);
+		}
+		&__image {
+			max-width: 100%;
+			max-height: 100%;
+		}
+	}
 </style>
